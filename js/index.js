f00c5a5efa2943d7d6c6c426c3cf5e11e314861d
soowan kim <tndhks1916@naver.com>
2022-10-13 14:01:41 +0900

    dd

--- js/index.js
+++ js/index.js
@@ -4,23 +4,21 @@ const testTwo = document.getElementsByClassName('my-page-dropdown')[0];
 const testThree = document.getElementsByClassName('search-btn-icon')[0];
 const testFour = document.getElementsByClassName('search-container')[0];
 
+
 showContents(testOne, testTwo);
 showContents(testThree, testFour);
 
 
-function showContents(eventBtn, contents){
+function showContents(btn, contents){
     // 버튼 클릭 이벤트로 display 스타일 조정
-    // 버튼 재클릭 or 닫기 클릭 or 컨텐츠 외부 영역 클릭 시 이벤트
+    // 버튼 재클릭 or 컨텐츠 외부 영역 클릭 시 이벤트
 
-    eventBtn.addEventListener('click', () => {
+    btn.addEventListener('click', () => {
         contents.classList.toggle('hide-box');
         console.log('버튼 클릭!');
     });
 
-    eventBtn.addEventListener('blur', () => {
-        contents.classList.add('hide-box');
-        console.log('외부영역');
-    });
+    //blur 이벤트 or 페이지 전체 클릭 이벤트 ??
 }
 
 
